2ffda0164df4ad8b5bf576ef21e1928877be9360
minagisyu <minagisyu@gmail.com>
2019-10-03 05:33:34 +0900

    ライブラリ名の変更
    プロジェクト名の変更
    libdl.so.2をlibdl.soに変更

--- FFmpeg.AutoGen/Native/LinuxNativeMethods.cs
+++ FFmpeg.AutoGen/Native/LinuxNativeMethods.cs
@@ -5,14 +5,15 @@ namespace FFmpeg.AutoGen.Native
 {
     public static class LinuxNativeMethods
     {
-        public const int RTLD_NOW = 0x002;
+		public const int RTLD_NOW = 0x002;
 
-        private const string Libdl = "libdl.so.2";
+	//	private const string Libdl = "libdl.so.2";
+		private const string Libdl = "libdl.so";
 
-        [DllImport(Libdl)]
-        public static extern IntPtr dlsym(IntPtr handle, string symbol);
+		[DllImport(Libdl)]
+		public static extern IntPtr dlsym(IntPtr handle, string symbol);
 
-        [DllImport(Libdl)]
-        public static extern IntPtr dlopen(string fileName, int flag);
+		[DllImport(Libdl)]
+		public static extern IntPtr dlopen(string fileName, int flag);
     }
 }
